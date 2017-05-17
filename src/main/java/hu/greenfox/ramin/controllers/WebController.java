1d8ac643221368c1c120f0adc291d072e4fc21eb
Ramin <RaminShamsi@yahoo.com>
2017-05-17 11:15:59 +0200

    add the local time which works on localhost

--- src/main/java/hu/greenfox/ramin/controllers/WebController.java
+++ src/main/java/hu/greenfox/ramin/controllers/WebController.java
@@ -6,15 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Controller
 public class WebController {
 
-  @RequestMapping("")
+  @GetMapping("")
   public String home(){
+    System.out.println(LocalDate.now().toString() + " " + LocalTime.now().toString());
     return "index";
   }
 
-  @GetMapping("/register")
+ // @PostMapping(value={"/","/"})
+
+  @PostMapping("/register")
   public String register(){
     return "register";
   }
